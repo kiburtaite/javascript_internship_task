8cb5d5bc3c8f6a52d8327c60a96a95856ad0dbd5
kiburtaite <kiburtaite@gmail.com>
2022-08-25 02:19:54 +0300

    getting company codes

--- server.js
+++ server.js
@@ -5,14 +5,19 @@ import finnhub from 'finnhub';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = process.env.API_KEY;
 const finnhubClient = new finnhub.DefaultApi();
 
-app.get('/results', async (req, res) => {
-    finnhubClient.symbolSearch('AAPL', (error, data, response) => 
-    res.json(data.result.map(result => result.symbol)))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/search', async (req, res) => {
+    finnhubClient.symbolSearch(req.body.text, (error, data, response) => {
+        const symbol = data.result.map(result => result.symbol);
+        res.json({codes: symbol})
+    })
 });
 
 app.get('/companies', async (req, res) => {
@@ -24,9 +29,5 @@ app.get('/candles', async (req, res) => {
     finnhubClient.stockCandles("AAPL", "D", 1640995200, 1654038000, (error, data, response) => 
     res.json(data))
 });
-  
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 const App = () => {
+
+const search = e => {
+  e.preventDefault();
+  fetch('http://localhost:5000/search', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({text: e.target.elements.text.value})
+  })
+  .then(res => res.json())
+  .then(data => console.log(data))
+};
+
   return (
     <div>
-      
+      <form onSubmit={search}>
+        <input
+        type="text"
+        name="text"
+        placeholder="Search for company by name"
+        />
+        <input
+        type="submit"
+        value="search"
+        />
+      </form>
     </div>
   );
 }
